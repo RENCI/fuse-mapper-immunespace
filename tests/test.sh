e476a71add27f914d29cafa9f8d240a00c1831f8
Kimberly Robasky <krobasky@gmail.com>
2021-03-12 16:13:42 -0500

    fixed testing framework to look like template

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -a
-source .env
-set +a
+export $(cat tests/test.env)
 
 docker-compose -f docker-compose.yml -f tests/docker-compose.yml up --build -V --exit-code-from fuse-mapper-immunespace
